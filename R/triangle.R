c9d27fc522e5202b578035d55f60c6cfc23b92f6
Siyu Sun <sunsiyu.tud@gmail.com>
2015-10-13 19:02:52 +0200

    add documentation.

--- R/triangle.R
+++ R/triangle.R
@@ -1,3 +1,12 @@
+#' Determine if multiple points are on one line
+#' 
+#' @param x  x coordinates of points
+#' @param y  y coordinates of points
+#' @return logical. TRUE indicates on one line
+#' @examples 
+#' isline(c(0,0), c(1,1)) # TRUE
+#' isline(1:3, rnorm(3))  # probably FALSE
+#' @export
 isline <- function(x, y)
 {
   stopifnot(is.numeric(x) && is.numeric(y))
@@ -6,6 +15,15 @@ isline <- function(x, y)
   return(do.call(identical, as.list(diff(y)/diff(x))))
 }
 
+#' Determine if a triangle is obtuse
+#' 
+#' @param x  x coordinates of vertices of a triangle
+#' @param y  y coordinates of vertices of a triangle
+#' @return logical. TRUE indicates the triangle is obtuse
+#' @examples 
+#' isobtuse(1:3, c(2, 8, 9)) # TRUE
+#' isobtuse(c(0,0,3), c(0,4,0))  # FALSE
+#' @export
 isobtuse <- function(x, y)
 {
   stopifnot(is.numeric(x) && is.numeric(y))
@@ -14,9 +32,18 @@ isobtuse <- function(x, y)
   
   d <- dist(cbind(x,y))
  
-  return (2*max(d) > sum(d))
+  return (2*max(d*d) > sum(d*d))
 }
 
+#' Determine if a triangle is right
+#' 
+#' @param x  x coordinates of vertices of a triangle
+#' @param y  y coordinates of vertices of a triangle
+#' @return logical. TRUE indicates the triangle is right
+#' @examples 
+#' isright(1:3, c(2, 8, 9)) # FALSE
+#' isright(c(0,0,3), c(0,4,0))  # TRUE
+#' @export
 isright <- function(x, y)
 {
   stopifnot(is.numeric(x) && is.numeric(y))
@@ -25,10 +52,19 @@ isright <- function(x, y)
   
   d <- dist(cbind(x,y))
   
-  return (2*max(d) == sum(d))
+  return (2*max(d*d) == sum(d*d))
 }
 
 
+#' Determine if a triangle is acute
+#' 
+#' @param x  x coordinates of vertices of a triangle
+#' @param y  y coordinates of vertices of a triangle
+#' @return logical. TRUE indicates the triangle is acute
+#' @examples 
+#' isacute(1:3, c(2, 8, 9)) # FALSE
+#' isacute(c(0,0,3), c(0,4,0))  # FALSE
+#' @export
 isacute <- function(x, y)
 {
   stopifnot(is.numeric(x) && is.numeric(y))
@@ -37,6 +73,6 @@ isacute <- function(x, y)
   
   d <- dist(cbind(x,y))
   
-  return (2*max(d) < sum(d))
+  return (2*max(d*d) < sum(d*d))
 }
   
