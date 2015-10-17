c5094adfafaa7580f096f056106bdfa25ccaa974
Siyu Sun <sunsiyu.tud@gmail.com>
2015-10-17 22:29:59 +0200

    add return value for plotting functions.

--- R/draw.R
+++ R/draw.R
@@ -1,3 +1,16 @@
+#' Plot circle with different methods
+#' 
+#' @param x,y coordinates of circle center
+#' @param r   radius of circle
+#' @param npoints number of points
+#' @param method  plotting method
+#' @param asp     aspect default is 1
+#' @param xlab,ylab x and y labels
+#' @param col color of circle
+#' @return invisible NULL
+#' @examples 
+#' plotcircle(0,0,1)
+#' @export
 plotcircle <- function(x=0, y=0, r=1, npoints=100, method="plot", 
                        asp=1, xlab="x", ylab="y", col="black", ...)
 {
@@ -16,6 +29,7 @@ plotcircle <- function(x=0, y=0, r=1, npoints=100, method="plot",
          polygon = {plot(rx, ry, asp=asp, xlab=xlab, ylab=ylab, type="n")
                     polygon(rx, ry, border=col, ...)}
          )
+  return(invisible(NULL))
 }
   
   
